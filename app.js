27c94ef6312f132725da76bf04712cb6b26a5940
Sachin Kelkar <sachinkel19@gmail.com>
2015-10-23 22:42:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl', [
+  '$scope',
+  function($scope){
+    $scope.test = 'Hello World!';
+    $scope.posts = [
+      'post 1',
+      'post 2',
+      'post 3'
+    ];
+  }
+]);
