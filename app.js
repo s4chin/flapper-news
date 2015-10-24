1deaa78c06f28744511249c1b309978a8d30099d
Sachin Kelkar <sachinkel19@gmail.com>
2015-10-24 17:06:56 +0530

    Add comments

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
+    $urlRouterProvider.otherwise('home');
+  }
+]);
 
 app.factory('posts', [function(){
   var o = {
-    posts: []
+    posts: [{title: 'Sunny Day', upvotes: 20, link: 'abc.def'}]
   };
   return o;
 }]);
@@ -17,7 +36,12 @@ app.controller('MainCtrl', [
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0});
+        upvotes: 0,
+        comments: [
+          {author: 'Billie', body: 'Come feed the rain', upvotes: 0},
+          {author: 'Jean', body: 'A distant ship, smoke o the horizon', upvotes: 0}
+        ]
+      });
       $scope.title = '';
       $scope.link = '';
     };
@@ -26,6 +50,24 @@ app.controller('MainCtrl', [
     };
     $scope.decrementUpvotes = function(post) {
       post.upvotes -= 1;
+    };
+  }
+]);
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
     };
   }
 ]);
