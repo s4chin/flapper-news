bf636d3d233ee71694e2a81e2d18a47fb290dd26
Sachin Kelkar <sachinkel19@gmail.com>
2015-10-24 12:06:53 +0530

    Add submit links

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
   '$scope',
   function($scope){
-    $scope.test = 'Hello World!';
     $scope.posts = [
       {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
+      {title: 'post 2', upvotes: 2, link: "https://www.google.com"},
       {title: 'post 3', upvotes: 15},
     ];
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === '') {return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0});
       $scope.title = '';
+      $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
