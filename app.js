1b7ca52107cdd0f0d7d4596dbf00b45e48585f42
Sachin Kelkar <sachinkel19@gmail.com>
2015-10-25 19:34:47 +0530

    Initial commit as per tutorial

--- app.js
+++ app.js
@@ -1,73 +1,60 @@
-var app = angular.module('flapperNews', ['ui.router']);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: '/home.html',
-        controller: 'MainCtrl'
-      })
-      .state('posts', {
-        url: '/posts/{id}',
-        templateUrl: '/posts.html',
-        controller: 'PostsCtrl'
-      });
-    $urlRouterProvider.otherwise('home');
-  }
-]);
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-app.factory('posts', [function(){
-  var o = {
-    posts: [{title: 'Sunny Day', upvotes: 20, link: 'abc.def'}]
-  };
-  return o;
-}]);
+var app = express();
 
-app.controller('MainCtrl', [
-  '$scope',
-  'posts',
-  function($scope, posts){
-    $scope.posts = posts.posts;
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') {return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Billie', body: 'Come feed the rain', upvotes: 0},
-          {author: 'Jean', body: 'A distant ship, smoke o the horizon', upvotes: 0}
-        ]
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };
-    $scope.decrementUpvotes = function(post) {
-      post.upvotes -= 1;
-    };
-  }
-]);
-app.controller('PostsCtrl', [
-  '$scope',
-  '$stateParams',
-  'posts',
-  function($scope, $stateParams, posts){
-    $scope.post = posts.posts[$stateParams.id];
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-    $scope.addComment = function(){
-      if($scope.body === '') { return; }
-      $scope.post.comments.push({
-        body: $scope.body,
-        author: 'user',
-        upvotes: 0
-      });
-      $scope.body = '';
-    };
-  }
-]);
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
