32eeb11ff296a671c699aa8eae2f79981785d862
Sachin Kelkar <sachinkel19@gmail.com>
2015-12-02 21:53:00 +0530

    Add upvote route

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,28 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post) {
+    if(err) { return next(err); }
+    if(!post) { return next(new Error('Can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if(err) { return next(err); }
+
+    res.json(post);
+  })
+});
+
 module.exports = router;
