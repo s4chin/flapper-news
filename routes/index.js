7da109635ac1228097c9473db6851e92fce59199
Sachin Kelkar <sachinkel19@gmail.com>
2015-12-04 00:16:25 +0530

    Add passport endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
+var passport = require('passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -89,6 +91,41 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 
     res.json(comment);
   })
+});
+
+// Register endpoint
+router.post('/register', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+  user.setPassword(req.body.password);
+
+  user.save(function(err) {
+    if(err) { return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+// Login endpoint
+router.post('/login', function(req, res, next) {
+  if(!req.body.username || !req.body.password) {
+    return res.status(400).json({message: 'Please fill out all the fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info) {
+    if(err) { return next(err); }
+
+    if(user) {
+      return res.json({ token: user.generateJWT() });
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
